f09442b9c51bc0a1a2d96a422f20466c34094743
reyreey <reyhaneh179@yahoo.com>
2025-01-14 21:43:49 +0330

    add PersonRoleInMediaItem repository and service

---
+++ src/main/java/com/reyreey/filimo/Repository/Content/IPersonRoleInMediaItemRepository.java
@@ -0,0 +1,12 @@
+package com.reyreey.filimo.Repository.Content;
+
+import com.reyreey.filimo.Model.Content.PersonRoleInMediaItem;
+
+/**
+ * @author : reyreey
+ * @mailto : reyhaneh179@yahoo.com
+ * @created : 1/14/2025, Tuesday
+ **/
+
+public interface IPersonRoleInMediaItemRepository extends IGenericRepository<PersonRoleInMediaItem> {
+}
