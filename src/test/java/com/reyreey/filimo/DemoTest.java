e929860dbf52879393591ca9c479b3ccb659621b
reyreey <reyhaneh179@yahoo.com>
2025-02-28 17:21:08 +0330

    add some annotations for dtos

---
+++ src/test/java/com/reyreey/filimo/DemoTest.java
@@ -0,0 +1,18 @@
+package com.reyreey.filimo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author : reyreey
+ * @mailto : reyhaneh179@yahoo.com
+ * @created : 2/14/2025, Friday
+ **/
+
+public class DemoTest {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
